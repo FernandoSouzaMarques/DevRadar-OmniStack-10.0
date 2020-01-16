8cf700f40ddf3b337891c93b5d5e24fed59fc937
fernandosouzamarques <fernando.souza@novahaus.com.br>
2020-01-16 13:37:10 -0300

    Add cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
+
 
 const app = express();
 
@@ -15,6 +17,7 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
