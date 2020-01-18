6ad0475a5ca7d8e3e6671fc2522a2d75b988b1cc
fernandosouzamarques <fernando.souza@novahaus.com.br>
2020-01-18 08:43:38 -0300

    Send message with socket for mobile in real time

--- api/src/controllers/devController.js
+++ api/src/controllers/devController.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async store(req, res) {
@@ -29,6 +30,13 @@ module.exports = {
         techs: techsArray,
         location,
       });
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray,
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
     return res.json(dev)
